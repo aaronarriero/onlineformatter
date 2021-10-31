f35e2e585e266ded4c44cca5e810cfdb9dbc8999
Arriero <aaron.arriero@paynopain.com>
2021-10-31 23:46:39 +0100

    Added links to the footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return (
             <footer className="page-footer">
-                About | Source | Contact
+                About | <a href="https://github.com/aaronarriero/onlineformatter" target="_blank" rel="noreferrer">Source</a> | <a href="mailto:aaronarriero@gmail.com">Contact</a>
             </footer>
         );
     }
